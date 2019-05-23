d5ca24cdb674a8165e2f895bf916ba57cb3f2a0d
jacekstamm <jacekstamm@gmail.com>
2019-05-23 14:31:26 +0200

    23.05.2019/5

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -55,9 +55,9 @@ public class TrelloClient {
                 .queryParam("key", trelloConfig.getTrelloAppKey())
                 .queryParam("token", trelloConfig.getTrelloToken())
                 .queryParam("name", trelloCardDto.getName())
-                .queryParam("description", trelloCardDto.getDescription())
+                .queryParam("desc", trelloCardDto.getDescription())
                 .queryParam("pos", trelloCardDto.getPos())
-                .queryParam("listId", trelloCardDto.getListId()).build().encode().toUri();
+                .queryParam("idList", trelloCardDto.getListId()).build().encode().toUri();
         LOGGER.info("restTemplate.postForObject url: " + url);
 
         return restTemplate.postForObject(url, null, CreatedTrelloCard.class);
