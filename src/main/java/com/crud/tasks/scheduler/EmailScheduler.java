41845e48585746d3b1809a2965ae7eefc70c803c
jacekstamm <jacekstamm@gmail.com>
2019-05-24 01:40:07 +0200

    HEroku update - mail Scheduled fixed

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -22,7 +22,7 @@ public class EmailScheduler {
 
     private static final String SUBJECT = "Tasks: Once a day e-mail.";
 
-    @Scheduled(fixedDelay = 10000)
+    @Scheduled(cron = "0 0 10 * * MON-FRI")
     public void sendInformationEmail() {
         long size = taskRepository.count();
         simpleEmailService.send(new Mail(
